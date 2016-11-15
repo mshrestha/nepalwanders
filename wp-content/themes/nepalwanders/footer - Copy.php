673597af03c9bd266532804c7a31eff946b0f43a
Nikesh <nikesh@kazistudios.com>
2016-11-15 13:01:43 +0545

    Initial commit. Nepal wanders moved from kazi270 to github.

---
+++ wp-content/themes/nepalwanders/footer - Copy.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nepalwanders
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nepalwanders' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nepalwanders' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nepalwanders' ), 'nepalwanders', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
